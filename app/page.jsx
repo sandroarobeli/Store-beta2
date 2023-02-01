97df65686512d70fe7a6746fb564aae36cd50d5c
Alex Arobelidze <sandroarobeli77@gmail.com>
2023-02-01 09:42:30 -0600

    Added Header, Footer to RootLayout, thus completing it. Also StoreProvider and SessionProvider

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="">
+    <main className="container m-auto mt-4 px-4">
       <h1 className="text-5xl text-cyan-700 font-orbitron">Boilerplate Code</h1>
     </main>
   );
